323df1cf558c1ce18b5955206576a6c4604ae1c5
Kirbs <kirbyust@gmail.com>
2016-09-25 12:16:55 +0800

    Hello World

---
+++ testingtheworld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  testingtheworld
+//
+//  Created by KIR CHENG on 25/9/2016.
+//  Copyright © 2016 kirost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
